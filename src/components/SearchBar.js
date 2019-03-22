8dd32b2deb356deaf5c583f8b02fb1120366264b
Khaled <disasterz9955@gmail.com>
2019-03-22 10:47:33 +0300

    Handle user input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,24 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  onInputChange(event) {}
+  state = { term: "" };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
